f29258f575bd8dc0c174ee849e844491d46aa1cf
dannephew <danneszhang@gmail.com>
2021-07-07 08:21:45 -0700

    edit assessment

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 const plusBtn = document.querySelector("#plus")
 const minusBtn = document.querySelector("#minus")
 const answer = document.querySelector(".answer")
+const insert = document.querySelector(".insert")
+let num
+insert.addEventListener("input", numFunction)
+function numFunction(e) {
+    console.log("numFunction")
+    num = Number(e.data)
+    console.log("num", num)
+}
 
 console.log(parseInt(answer.textContent))
+
 plusBtn.onclick = function() {
-    answer.textContent = parseInt(answer.textContent) + 1
+    answer.textContent = parseInt(answer.textContent) + num
 }
 
 minusBtn.onclick = function() {
-    answer.textContent = parseInt(answer.textContent) - 1
+    answer.textContent = parseInt(answer.textContent) - num
 }
